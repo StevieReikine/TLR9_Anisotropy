f47d8fdb59fc46a8de3e2097ac816df409b56c28
StevieReikine <37851532+StevieReikine@users.noreply.github.com>
2020-04-17 20:04:02 +0100

    files prettied up
    comments added, _twosites rewritten properly with a function, function renamed

--- Kd_two-sites.py
+++ Kd_two-sites.py
@@ -3,61 +3,63 @@ from scipy.optimize import fsolve
 import random as rdm
 import numpy as np
 
-def equations(x):
-    P, D, PD1, PD2, PDD = x
-    eq1 = P*D/PD1 - K1
-    eq2 = P*D/PD2 - K2
-    eq3 = P*D*D/(K1*K2) - PDD
-    eq4 = P + PD1 + PD2 + PDD - P0
-    eq5 = D + PD1 + PD2 + 2*PDD - D0
-    return (eq1, eq2, eq3, eq4, eq5)
+# this function simulates what the Anisotropy would be for a value of P0, Ka, and Kb
+def Anisotropy(X, K_a, K_b):
+    # variable to test whether a solution to the set of equations was found (to be used after fsolve)
+    SolutionFound = False
+    # set up parameters of system
+    Y = [0]*len(X)
+    Af = 58.8
+    Ab = 186.1
+    i = 0
+    for P0 in X:
+        def equations(x):
+            P, D, PD1, PD2, PDD = x
+            eq1 = P*D/PD1 - K_a
+            eq2 = P*D/PD2 - K_b
+            eq3 = P*D*D/(K_a*K_b) - PDD
+            eq4 = P + PD1 + PD2 + PDD - P0
+            eq5 = D + PD1 + PD2 + 2*PDD - D0
+            return (eq1, eq2, eq3, eq4, eq5)
+        for j in range(100):
+                P, D, PD1, PD2, PDD = fsolve(equations, (rdm.randint(0,P0),1,0.5,rdm.randrange(0,D0),0.5),maxfev=50000)
+                Ff = D/D0                       # definition of Fraction Free D
+                Fb = (PD1 + PD2 + 2*PDD)/D0     # definition of Fraction Bound D
+                F = Ff + Fb                     # F must be 1
+                A = Af*Ff + Ab*Fb               # Definition of Anisotropy
+                # check that solutions found meet physical constrains, e.g. F = 1, no negative values, and Af < A < Ab                
+                if(F>0.9999 and F<1.0001 and P>0 and D>0 and PD1>0 and PD2>0 and PDD>0 and A > Af and A < Ab):
+                    SolutionFound = True
+                    break
+        Y[0] = Af       # by definition, so do not update Y[0] based on fit generated above
+        if i > 0:   
+            # update Y = A if conditions met
+            if SolutionFound:
+                Y[i] = A
+            # in case a solution is not found, update Y with previous value of A- this is not ideal but it generates a value of A for every P0 and for most values of P0, Y will be close
+            else:
+                Y[i] = Y[i-1]
+        i = i + 1
+    return(Y)                                       
 
-K1 = 20
-K2 = 40
+# set desired values of Ka and Kb to simulate
+K_a = 20
+K_b = 40
+
+# set value of D0
 D0 = 2
-X = []
-Y = []
-for P0 in range (0,20,1):
-    for i in range(10000):
-        P, D, PD1, PD2, PDD = fsolve(equations, (rdm.randint(0,P0),1,0.5,rdm.randrange(0,D0),0.5),maxfev=50000)
-        #P, D, PD, PDD, PPD, P2D2, P2 = fsolve(equations,(P0,D0,D0,D0,D0,D0,5),maxfev=50000)
-        #print(P0)
-        Ff = D/D0
-        Fb = (PD1 + PD2 + 2*PDD)/D0
-        F = Ff + Fb
-        if(F>0.999999999999 and F<1.00000000001):
-        #if(P>0 and D>0 and PD>0 and PDD>0 and PPD>0 and P2D2>0 and P2>0):
-                print(Ff + Fb)
-                #print(equations((P, D, PD, PDD, PPD, P2D2, P2)))
-                break
-    Af = 58.8
-    Ab = 186.1
-    A = Af*Ff + Ab*Fb
-    print(A)
-    X.append(P0)
-    Y.append(A)
 
-for P0 in range (20,100,10):
-    for i in range(10000):
-        P, D, PD1, PD2, PDD = fsolve(equations, (rdm.randint(0,P0),1,0.5,rdm.randrange(0,D0),0.5),maxfev=50000)
-        #P, D, PD, PDD, PPD, P2D2, P2 = fsolve(equations,(P0,D0,D0,D0,D0,D0,5),maxfev=50000)
-        #print(P0)
-        Ff = D/D0
-        Fb = (PD1 + PD2 + 2*PDD)/D0
-        F = Ff + Fb
-        if(F>0.999999999999 and F<1.00000000001):
-        #if(P>0 and D>0 and PD>0 and PDD>0 and PPD>0 and P2D2>0 and P2>0):
-                print(Ff + Fb)
-                #print(equations((P, D, PD, PDD, PPD, P2D2, P2)))
-                break
-    Af = 58.8
-    Ab = 186.1
-    A = Af*Ff + Ab*Fb
-    print(A)
-    X.append(P0)
-    Y.append(A)
+# set range of x values: 0 to 20 in steps of 1 then 20 to 100 in steps of 10
+X1 = list(range(0,20))
+X2 = list(range(20,101, 10))
+X = X1 + X2
 
+# simulate anisotropy data for given X, Ka, and Kb    
+Y = Anisotropy(X, K_a, K_b)
+
+# plot the data
 plt.scatter(X,Y)
 plt.show()
 
-np.savetxt('Kd-test-20-40_b.csv', (X, Y), delimiter=',')
+# save the simulated data as a vs file
+np.savetxt('Kd-test-20-40_d.csv', (X, Y), delimiter=',')
--- Kd_two-sites_K1andK4only.py
+++ Kd_two-sites_K1andK4only.py
@@ -3,55 +3,65 @@ from scipy.optimize import fsolve
 import random as rdm
 import numpy as np
 
-def equations(x):
-    P, D, PD, P2D2 = x
-    eq1 = P*D/PD - K1
-    eq2 = PD*PD/P2D2 -K4
-    eq3 = P + PD + 2*P2D2 - P0
-    eq4 = D + PD + 2*P2D2 - D0
-    return (eq1, eq2, eq3, eq4)
-
-
-X1 = list(range(0,20))
-X2 = list(range(20,110, 10))
+# Set values for X, first 0-1 in steps of 0.1 then up to 100 in steps of 1
+X1 = [x*0.1 for x in range(1, 10)]
+X2 = list(range(1,101))
 X = X1 + X2
-Y = [0]*len(X)
 
-def MyFuntion(P0, X, Y):
+# this function simulates what the Anisotropy would be for a value of P0, K1, and K4
+def Anisotropy(X, K1, K4):
+    # variable to test whether a solution to the set of equations was found (to be used after fsolve)
     SolutionFound = False
-    for i in range(100):
-        P, D, PD, P2D2 = fsolve(equations, (rdm.randint(0,P0),rdm.randrange(0,D0),rdm.randrange(0,D0),rdm.randrange(0,D0)),maxfev=50000)
-        #P, D, PD, PDD, PPD, P2D2, P2 = fsolve(equations,(P0,D0,D0,D0,D0,D0,5),maxfev=50000)
-        #print(P0)
-        Ff = D/D0
-        Fb = (PD + 2*P2D2)/D0
-        F = Ff + Fb
-        if(F>0.999999999999 and F<1.00000000001 and P>0 and D>0 and PD>0 and P2D2>0):
-        #if(P>0 and D>0 and PD>0 and PDD>0 and PPD>0 and P2D2>0 and P2>0):
-                print(Ff + Fb)
-                #print(equations((P, D, PD, PDD, PPD, P2D2, P2)))
-                SolutionFound = True
-                break
-    Af = 180
-    Ab = 250
-    A = Af*Ff + Ab*Fb
-    print(A, P, D, PD, P2D2)
-    X.append(P0)
-    Y.append(A)
+    # set up parameters of system
+    Y = [0]*len(X)      # generate list of Y values, initiallized to 0
+    Af = 130            # Free Anisotropy (this is empirical- set to match data)
+    Ab = 250            # Bound Anisotropy (this is empirical- set to match data)
+    i = 0
+    for P0 in X:
+        # define the set of equations that governs the interaction being modeled 
+        def equations(x):
+            P, D, PD, P2D2 = x
+            eq1 = P*D/PD - K1               # K1 = [P]*[D]/[PD]
+            eq2 = PD*PD/P2D2 - K4           # K4 = [PD]^2/[P2D2]
+            eq3 = P + PD + 2*P2D2 - P0      # P0 = P + PD + 2*P2D2   (conservation of mass)
+            eq4 = D + PD + 2*P2D2 - D0      # D0 = D + PD + 2*P2D2   (conservation of mass)
+            return (eq1, eq2, eq3, eq4)
+        for j in range(100):
+            # solve the equation to get values of P, D, PD, and P2D2 
+            # it is useful to chose physically reasonable initial values: e.g. P must be between 0 and P0
+            P, D, PD, P2D2 = fsolve(equations, (rdm.uniform(0,P0),D0,rdm.randrange(0,D0),0.5),maxfev=50000)
+            Ff = D/D0                   # definition of Fraction Free D
+            Fb = (PD + 2*P2D2)/D0       # definition of Fraction Bound D
+            F = Ff + Fb                 # F must be 1
+            A = Af*Ff + Ab*Fb           # Definition of Anisotropy
+            # check that solutions found meet physical constrains, e.g. F = 1, no negative values, and Af < A < Ab
+            if(F>0.99 and F<1.01 and P>0 and D>0 and PD>0 and P2D2>0 and A > Af and A < Ab):
+                    SolutionFound = True
+                    break
+        Y[0] = Af   # by definition, so do not update Y[0] based on fit generated above
+        if i > 0:
+            # update Y = A if conditions met
+            if SolutionFound:
+                Y[i] = A
+            # in case a solution is not found, update Y with previous value of A- this is not ideal but it generates a value of A for every P0 and for most values of P0, Y will be close
+            else:
+                Y[i] =Y[i-1]
+        i = i + 1
+    return(Y)
 
-K1 = 20
-K4 = 40
+# set values for K1 and K4
+K1 = 7
+K4 = 2
+
+# value for D0
 D0 = 5
-X = []
-Y = []
 
-for P0 in range (0,20,1):
-    MyFuntion(P0, X, Y)
+# simulate Anisotropy data for desired range of X and given K1, K4
+Y = Anisotropy(X, K1, K4)
 
-for P0 in range (20,100,10):
-    MyFuntion(P0, X, Y)
-
+# plot the data
 plt.scatter(X,Y)
 plt.show()
 
-np.savetxt('Kd-test-K1-K4_b.csv', (X, Y), delimiter=',')
+# save the simulated data in a sv file
+np.savetxt('Kd-test-K1-K4_c.csv', (X, Y), delimiter=',')
